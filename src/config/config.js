1134bbdd1e633a1c86881cb0224e4790818955a1
AdithyasacharyaWorks <156585313+AdithyasacharyaWorks@users.noreply.github.com>
2024-06-25 12:13:41 +0530

    change of config url

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const baseUrl = {
     development: 'http://localhost:3000',
-    production: 'https://taskpair-hj0xf61p9-adithya-s-acharyas-projects.vercel.app',
+    production: 'https://task-pair-1rwz.vercel.app/',
 };
 export default baseUrl;
