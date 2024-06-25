1ac695f15a5adbb1d223430cdc32451ecde87211
AdithyasacharyaWorks <156585313+AdithyasacharyaWorks@users.noreply.github.com>
2024-06-25 12:07:34 +0530

    changed url for deployment

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const baseUrl = {
     development: 'http://localhost:3000',
-    production: 'https://taskpair.vercel.app',
+    production: 'https://taskpair-hj0xf61p9-adithya-s-acharyas-projects.vercel.app',
 };
 export default baseUrl;
